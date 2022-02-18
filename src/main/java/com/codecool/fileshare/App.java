40ef50958f36ceb1daf7ae2974c840814deeb444
Csáki P <Peter>
2022-02-18 03:11:40 +0100

    menu

---
+++ src/main/java/com/codecool/fileshare/App.java
@@ -0,0 +1,12 @@
+package src.main.java;
+
+import input.Menu;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        new Menu().mainMenu();
+
+    }
+}
--- src/main/java/com/codecool/fileshare/FileshareApplication.java
+++ src/main/java/com/codecool/fileshare/FileshareApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FileshareApplication {
 
-    public static void main(String[] args) {
-        SpringApplication.run(FileshareApplication.class, args);
-    }
+   // public static void main(String[] args) {
+  //      SpringApplication.run(FileshareApplication.class, args);
+  //  }
 
 }
